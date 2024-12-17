627c9e44b825b771da58ef6dfa39693a987eaf2f
aaronkychan <110659061+universallyleo@users.noreply.github.com>
2024-12-18 00:24:44 +0900

    Try fix Github Action

--- .svelte-kit/generated/client/nodes/0.js
+++ .svelte-kit/generated/client/nodes/0.js
@@ -1,1 +1,3 @@
+import * as universal from "../../../../src/routes/+layout.js";
+export { universal };
 export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@2.7.2_@sveltejs+vite-plugin-svelte@4.0.0_svelte@5.1.1_vite@5.4.10__svelte@5.1.1_vite@5.4.10/node_modules/@sveltejs/kit/src/runtime/components/svelte-5/layout.svelte";
